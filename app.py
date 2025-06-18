372f6fb7d00d5b5346bcb89f4669f3047ea6862d
samanyuahuja <samanyuahuja@gmail.com>
2025-06-18 21:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Step 1: Install necessary libraries
 # Step 1: Install necessary libraries
 # Step 1: Install necessary libraries
-!pip install streamlit shap lime joblib pandas numpy scikit-learn matplotlib seaborn
+
 
 import streamlit as st
 import pandas as pd
