5a5edd449e3aff1eb749b09f5e1616ba9c67fe8b
samanyuahuja <samanyuahuja@gmail.com>
2025-07-01 19:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ from sklearn.inspection import PartialDependenceDisplay
 
 @st.cache_resource
 def load_resources():
-    model = joblib.load("model (6).pkl")
-    scaler = joblib.load("scaler (5).pkl")
+    model = joblib.load("model (7).pkl")
+    scaler = joblib.load("scaler (6).pkl")
     try:
-        X_train_scaled = joblib.load("X_train_scaled.pkl")
+        X_train_scaled = joblib.load("X_train_scaled (1).pkl")
     except Exception as e:
         st.warning(f"X_train_scaled failed to load: {e}")
         X_train_scaled = None
